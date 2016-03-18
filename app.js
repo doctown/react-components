da8465282a8655cd55d045b66fa0f21b41abc812
David Ogor <doctown@yahoo.com>
2016-03-18 10:40:23 -0700

    Add components in grocery list

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    <Ham />
+    <Eggs />
+  </ul>
+);
+
+var Ham = () => (
+  <li>Ham</li>
+);
+
+var Eggs = () => (
+  <li>Eggs</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
